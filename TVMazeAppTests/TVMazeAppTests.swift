8feebeb09bc5a07d3c161d63148f7aa00b481e75
Fábio Augusto Resende <fabioresende.prog@gmail.com>
2025-08-10 02:18:30 -0300

    Initial Commit

---
+++ TVMazeAppTests/TVMazeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TVMazeAppTests.swift
+//  TVMazeAppTests
+//
+//  Created by Fabio Augusto Resende on 8/10/25.
+//
+
+import Testing
+@testable import TVMazeApp
+
+struct TVMazeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
